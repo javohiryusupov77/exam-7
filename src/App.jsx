8a98bcaafb51c9c5c8f39b9ac81a791f4ab812cb
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-07-17 18:28:07 +0500

    commit 2

---
+++ src/App.jsx
@@ -0,0 +1,205 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import ImageOfHeader from "./assets/GG.svg";
+import ArrowButtom from "./assets/arrow-button.svg";
+import ImageOfUnited from "./assets/United.svg";
+import vector from "./assets/Vector.svg";
+import GameGeek from "./assets/GameGeek.svg";
+import styled from "styled-components";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Home from "./components/home/Home";
+import Products from "./components/products/Products";
+import Product from "./components/product/Product";
+import { FaCartShopping } from "react-icons/fa6";
+
+function App() {
+  const [cart, setCart] = useState([]);
+  const [add, setAdd] = useState(0);
+  const baseURL = "https://headphones-server.onrender.com/products";
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(baseURL);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, [add, baseURL]);
+
+const FooterContainer = styled.footer`
+  background-color: #000;
+  color: #fff;
+  padding: 20px 0;
+  flex-direction: column;
+  align-items: center;
+  max-width:1278px;
+  margin:auto
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  @media (max-width: 500px) {
+    flex-direction: column;
+  }
+`;
+
+const Section = styled.div`
+  flex: 1;
+  margin: 20px;
+  h3 {
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 1.2rem;
+    margin: 5px 0;
+    color: #ccc;
+  }
+`;
+
+  return (
+    <div>
+      <Router>
+        <header>
+          <div className="container-1">
+            <div>
+              <div className="content-1">
+                <img className="logo" src={ImageOfHeader} alt="logo" />
+                <p>90 510-71-44</p>
+              </div>
+            </div>
+            <div>
+              <div className="content-2">
+                <p>
+                  Get 50% Off on the Selected items <span>|</span>
+                </p>
+                <p>Shop now</p>
+              </div>
+            </div>
+            <div>
+              <div className="content-3">
+                <div className="ArrowButtom">
+                  <img
+                    style={{ cursor: "pointer" }}
+                    src={ArrowButtom}
+                    alt="buttomOfImage"
+                  />
+                  <p>English</p>
+                  <img className="flag" src={ImageOfUnited} alt="flag" />
+                </div>
+                <div className="worldIcon">
+                  <img
+                    style={{ width: "17px", height: "17px" }}
+                    src={vector}
+                    alt="vector"
+                  />
+                  <p>Location</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container-2">
+            <div>
+              <img className="GameGeek" src={GameGeek} alt="logo" />
+            </div>
+            <div className="navbar-container">
+              <nav className="navbar">
+                <ul className="header-link">
+                  <li>
+                    <NavLink
+                      className={({ isActive }) =>
+                        isActive ? "activeLink" : ""
+                      }
+                      to="/"
+                    >
+                      Cart
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      className={({ isActive }) =>
+                        isActive ? "activeLink" : ""
+                      }
+                      to="/products"
+                    >
+                      Products
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink>Help</NavLink>
+                  </li>
+                </ul>
+                <NavLink to="/">
+                  <div className="cart-icon">
+                    <FaCartShopping />
+                    {add > 0 && <span className="cart-count">{add}</span>}
+                  </div>
+                </NavLink>
+              </nav>
+            </div>
+          </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home cart={cart} />} />
+          <Route
+            path="/products"
+            element={<Products cart={cart} setCart={setCart} setAdd={setAdd} />}
+          />
+          <Route path="/products/:productId" element={<Product />} />
+        </Routes>
+      </Router>
+      <FooterContainer>
+        <FooterContent>
+          <Section>
+            <h3>Services</h3>
+            <p>Gift card</p>
+            <p>Mobile app</p>
+            <p>Shipping & Delivery</p>
+            <p>Order Pickup</p>
+            <p>Account Signup</p>
+          </Section>
+          <Section>
+            <h3>Help</h3>
+            <p>ShopCart Help</p>
+            <p>Returns</p>
+            <p>Track Orders</p>
+            <p>Feedback</p>
+            <p>Security & Fraud</p>
+          </Section>
+          <Section>
+            <h3>About us</h3>
+            <p>News & Blog</p>
+            <p>Help</p>
+            <p>Press Center</p>
+          </Section>
+          <Section>
+            <h3>About me</h3>
+            <p>Created by Javohir Yusupov </p>
+            <p>17.07.2024</p>
+            <p>Link t.me JavohirYusupov77</p>
+          </Section>
+        </FooterContent>
+      </FooterContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styles from "./Product.module.scss";
+import Swal from "sweetalert2";
+
+const baseURL = import.meta.env.VITE_BASE_URL;
+
+const Product = () => {
+  const { productId } = useParams();
+
+  const [product, setProduct] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchProductById() {
+      const response = await fetch(`${baseURL}/products/${productId}`);
+      const data = await response.json();
+      setProduct(data);
+    }
+
+    fetchProductById();
+  }, [productId]);
+  const handleBack = () => {
+    navigate(-1);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      },
+    });
+    Toast.fire({
+      icon: "success",
+      title: "We are in products page",
+    });
+  };
+
+  return (
+    <>
+      <button className={styles.backButton} onClick={() => handleBack()}>
+        back products
+      </button>
+      <div className={styles.product}>
+        {product && (
+          <>
+            <h2>{product.name}</h2>
+            <img src={product.image_url} alt={product.name} />
+            <p>{product.description}</p>
+            <p className={styles.price}>Price: ${product.price}</p>
+            <p className={styles.category}> Brand{product.brand_name}</p>
+            <div className={styles.productDetails}>
+              <strong>{product.description}</strong>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Product;
